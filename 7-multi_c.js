34ae402a9194b4581f9ee2968558881888457647
Finidi47 <finidisaid@gmail.com>
2025-05-25 22:40:51 +0300

    It works on my machine

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,11 @@
 const arguments = process.argv
 
 let value_1 = parseInt(arguments[2]);
-const language = 'C is fun';
 
 if (isNaN(value_1)){
     console.log('Missing number of occurrences');
-} 
-
-for(i=0; i < value_1; i++){
-        console.log(language);
+} else {
+    for(let i = 0; i < value_1; i++){
+        console.log('C is fun');
     }
+}
