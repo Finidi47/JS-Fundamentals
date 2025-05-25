310c6515ff766e25ad1320592a7c5fcb2e21d666
Finidi47 <finidisaid@gmail.com>
2025-05-25 22:18:46 +0300

    I love C

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const arguments = process.argv
+
+let value_1 = parseInt(arguments[2]);
+const language = 'C is fun';
+
+if (isNaN(value_1)){
+    console.log('Missing number of occurrences');
+} else{
+    for(i=0; i < value_1; i++){
+        console.log(language);
+    }
+}
